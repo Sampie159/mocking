c7146a1692224899da613a3a807ad992ea091083
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-04-21 15:36:55 -0300

    First draft, probably won't change

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let text = '';
+
+	const mock = (text: string): string => {
+		let upper = true;
+		let out = '';
+		for (let i = 0; i < text.length; i++) {
+			if (text.charAt(i) === ' ') out += ' ';
+			else {
+				if (upper) {
+					out += text.charAt(i).toUpperCase();
+					upper = !upper;
+				} else {
+					out += text.charAt(i).toLowerCase();
+					upper = !upper;
+				}
+			}
+		}
+		return out;
+	};
+</script>
+
+<div class="bg-slate-500 flex flex-col h-screen">
+	<p class="self-center font-bold text-slate-900">Enter text here!</p>
+	<textarea class="mx-9 h-56 font-bold" name="henlo" bind:value={text} id="henlo" />
+	<p class="self-center font-bold text-slate-900">See the output here</p>
+	<p class="mx-9 bg-white font-bold h-56">{mock(text)}</p>
+</div>
